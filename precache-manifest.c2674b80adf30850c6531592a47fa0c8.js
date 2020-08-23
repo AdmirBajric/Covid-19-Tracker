a2143498e68990543b81b20af28f2840557c88f4
Admir Bajric <admir.bajric.dev@gmail.com>
2020-08-23 18:30:49 +0200

    Updates

---
+++ precache-manifest.c2674b80adf30850c6531592a47fa0c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef1e36558877259b6951f684e065ee3",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "e6e06b8de06429449569",
+    "url": "/Covid-19-Tracker/static/css/main.fb698b3d.chunk.css"
+  },
+  {
+    "revision": "a37d6d83561eff6db64f",
+    "url": "/Covid-19-Tracker/static/js/2.486219ec.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-19-Tracker/static/js/2.486219ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e06b8de06429449569",
+    "url": "/Covid-19-Tracker/static/js/main.4aa2495d.chunk.js"
+  },
+  {
+    "revision": "85f1ace4262ca4f88866",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.03b45b31.js"
+  }
+]);
